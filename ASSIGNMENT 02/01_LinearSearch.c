c5ccc1c5288211189fd3cfe131ed0d2b0109e3ac
Alok Kumar <140330466+ALOK-CST@users.noreply.github.com>
2024-10-26 23:41:15 +0530

    Update 01_LinearSearch.c

--- ASSIGNMENT 02/01_LinearSearch.c
+++ ASSIGNMENT 02/01_LinearSearch.c
@@ -1,1 +1,26 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,key;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int *arr= (int*)malloc(n*sizeof(int));
+    printf("Enter the numbers: ");
+    for (int x =0;x<n;x++)
+    {
+        scanf("%d",&arr[x]);
+    }
+    printf("Enter the number to be searched: ");
+    scanf("%d",&key);
+    for(int x= 0;x<n;x++)
+    {
+        if(arr[x]==key)
+        {
+            printf("%d is found and is present at %d position",key,x+1);
+            return 0;
+        }
+    }
+    printf("%d is not present in the array",key);
+    free(arr);
+}
